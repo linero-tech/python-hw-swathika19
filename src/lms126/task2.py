27648ad174ac54b832d93521cd5ef7db22df6069
swathika19 <swathikagp@gmail.com>
2023-02-02 13:22:56 +0100

    Solution task2

--- src/lms126/task2.py
+++ src/lms126/task2.py
@@ -2,4 +2,9 @@ from to_do import TODO
 
 
 def task2():
-    return TODO("Erase this line and write your solution")
+    return """
+     Take the input of base of the rectangle
+     Take the input of  height of the rectangle
+     Calculate the area of rectangle using the formula base*height      
+     Display output
+    """
