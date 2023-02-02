9c463f95f9c7c5c4891c0e3543006d0de2158065
swathika19 <swathikagp@gmail.com>
2023-02-02 13:35:44 +0100

    Solution Task3

--- src/lms126/task3.py
+++ src/lms126/task3.py
@@ -2,4 +2,9 @@ from to_do import TODO
 
 
 def task3():
-    return TODO("Erase this line and write your solution")
+    return """"
+    Input of the base of the Triangle
+    Input of the height of the Triangle
+    Calculate the Area of the Triangle using the formula 0.5*base*height of the triangle
+    Print the output of the Triangle
+    """
