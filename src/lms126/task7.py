da5d95decdc5441d962bf55a6590fbae69d05bc4
swathika19 <swathikagp@gmail.com>
2023-02-07 13:06:18 +0100

    Solution Task7

--- src/lms126/task7.py
+++ src/lms126/task7.py
@@ -2,4 +2,10 @@ from to_do import TODO
 
 
 def task7():
-    return TODO("Erase this line and write your solution")
+
+    return """
+    Take Input number as x
+    For numbers  from 1 to 100
+    if the value of x divided by 2 equals to 0 then
+    print Output x
+    """
