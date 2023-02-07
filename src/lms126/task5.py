e8e6596a0330763302b4fd5011acdad5c0456134
swathika19 <swathikagp@gmail.com>
2023-02-07 12:32:51 +0100

    Solution Task5

--- src/lms126/task5.py
+++ src/lms126/task5.py
@@ -2,4 +2,21 @@ from to_do import TODO
 
 
 def task5():
-    return TODO("Erase this line and write your solution")
+    return """
+    Take the input as the value of x
+    For numbers from 1 to 8
+    if x value equals to 1 then 
+    print output as Monday 
+    else check if x value equals to 2 then 
+    print output as Tuesday
+    else check if x value equals to 3 then
+    print output as Wednesday
+    else check if x value equals to 4 then
+    print output as Thursday
+    else check if x value equals to 5 then
+    print output as Friday
+    else check if x value equals to 6 then
+    print output as saturday
+    else check if x value equals to 7 then
+    print output as Sunday
+    """
