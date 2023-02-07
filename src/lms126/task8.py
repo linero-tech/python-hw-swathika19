1c19f96fb29a62748fbadf65603de580131fa11e
swathika19 <swathikagp@gmail.com>
2023-02-07 13:43:22 +0100

    Solution Task8

--- src/lms126/task8.py
+++ src/lms126/task8.py
@@ -2,4 +2,14 @@ from to_do import TODO
 
 
 def task8():
-    return TODO("Erase this line and write your solution")
+    return """
+    Take Input value as a number for x
+    check while the reminder of x divided by 3 equals to zero and reminder of x divided by 5 equals to zero or five 
+    then Print Output as "FooBar"
+    check while the reminder of x divided by 3 not equals to zero and reminder of x divided by 5 not equals to zero or five 
+    then Print Output as "Qix"
+    If x divided by 3 equals to zero then
+    print Output as "Foo"
+    else check If x divided by 5 equals to zero or 5 then 
+    print Output as "Bar"
+    """
