9290b687043298fe6c5c5c3e0686cbce43ecc214
swathika19 <swathikagp@gmail.com>
2023-02-07 12:48:32 +0100

    Solution Task5

--- src/lms126/task6.py
+++ src/lms126/task6.py
@@ -2,4 +2,12 @@ from to_do import TODO
 
 
 def task6():
-    return TODO("Erase this line and write your solution")
+    return """
+     Input the value of number to x
+     For numbers 1 to 100
+     check using while condition 
+     whether the remainder value of x is divided by 5 equals to zero or 5
+     then print output "x is multiple of  5 "  
+     else print output "x is not multiple of 5"   
+    
+    """
