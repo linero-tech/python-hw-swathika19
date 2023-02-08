6f745e4e7c21e31c062cb052928e442feb4c8bbf
swathika19 <swathikagp@gmail.com>
2023-02-08 15:28:01 +0100

    SolutionTask10

--- src/lms126/task10.py
+++ src/lms126/task10.py
@@ -2,4 +2,15 @@ from to_do import TODO
 
 
 def task10():
-    return TODO("Erase this line and write your solution")
+    return """
+    Input the email id to emailId string
+    check the emailId has @, .com,.se , it should not have any blank spaces
+    emailId is also should not be repetitive
+    Input the Password as a string
+    check the password length.It should be greater than 6.
+    check both the conditions of email and password
+    then create the Account
+    
+    
+    
+    """
