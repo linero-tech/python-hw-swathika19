475008d04fbb087a0abd3615802d8eec347f29ee
swathika19 <swathikagp@gmail.com>
2023-02-08 14:44:05 +0100

    Solution Task9

--- src/lms126/task9.py
+++ src/lms126/task9.py
@@ -2,4 +2,15 @@ from to_do import TODO
 
 
 def task9():
-    return TODO("Erase this line and write your solution")
+    return """
+    Instert the ATM card in the machine.
+    select the language.
+    Display the PIN number to be entered
+    select the Transaction type
+    select Enter amount to withdraw
+    Input the Amount to be withdrawn
+    ATM machine give sthe amount
+    Balance= Balance-Amount
+    Display the Balance Amount
+    
+    """
