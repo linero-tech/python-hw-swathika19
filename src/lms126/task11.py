89324a7ca5b9df277f896078a5d03657adf33bea
swathika19 <swathikagp@gmail.com>
2023-02-08 13:08:11 +0100

    Solution task11

--- src/lms126/task11.py
+++ src/lms126/task11.py
@@ -2,4 +2,17 @@ from to_do import TODO
 
 
 def task11():
-    return TODO("Erase this line and write your solution")
+    return """
+    Enter the input value to be converted into a variable tempinput
+    check IF the variable tempinput contains a character c
+    then we have to convert into Fareinheit 
+    take only numbers from tempinput into tempinputn
+    tempoutput=(tempinputn x 1.8) + 32 
+    print tempoutput+F
+    else
+    if the tempinput contains a character F
+    then we have to convert to celsius
+    Take only numbers from tempinput into tempinputn
+    tempoutput= (tempinputn - 32) x .5556 
+    Print tempoutput+C
+    """
