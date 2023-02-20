e78d5420d7170887fd76c6ebb3f37425772ed58c
swathika19 <swathikagp@gmail.com>
2023-02-20 17:46:16 +0100

    solution task3

--- src/lms127/task3.py
+++ src/lms127/task3.py
@@ -2,10 +2,10 @@ from to_do import TODO
 
 
 def task3(radius):
-    perimeter = 2*3.14*radius
+    perimeter = 2*3.141*radius
     return perimeter
 
 if __name__ == "__main__":
-    perimeter = task3(radius=4.0)
-    print(perimeter)
 
+
+    print(task3(radius=5.0))
