0b9af447f9c9156d605d75ae7f43723cbc7f3089
swathika19 <swathikagp@gmail.com>
2023-02-22 12:17:35 +0100

    Solution task6

--- src/lms127/task6.py
+++ src/lms127/task6.py
@@ -2,6 +2,11 @@ from to_do import TODO
 
 
 def task6(base1, base2, height):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+
+    area = 0.5*( base1 + base2)*height
+    result = area
+    return result
+
+if __name__ == "__main__":
+
+        print(task6(base1=30.0, base2= 10.0, height=5.0))
