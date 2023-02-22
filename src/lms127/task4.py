09d7b085150e57485c8c230ff806544cdc42fb7f
swathika19 <swathikagp@gmail.com>
2023-02-22 12:55:14 +0100

    update task4

--- src/lms127/task4.py
+++ src/lms127/task4.py
@@ -4,8 +4,9 @@ from to_do import TODO
 def task4(base, height):
     area = 0.5*base*height
     result = area
+    print(result)
     return result
 
 
 if __name__ == "__main__":
-    print(task4(base=20.0, height=5.0))
+    task4(base=20.0, height=5.0)
